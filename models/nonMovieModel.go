51fae618876679d7ec629d6a2f4eb1c800b36332
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-07-31 14:06:21 +0800

    remove ID field

--- models/nonMovieModel.go
+++ models/nonMovieModel.go
@@ -1,17 +1,12 @@
 package models
 
-import (
-	"github.com/globalsign/mgo/bson"
-)
-
 type NonMovie struct {
-	ID       bson.ObjectId `bson:"_id"`
-	Title    string        `json:"title"`
-	Desc     string        `json:"desc"`
-	Location string        `json:"location"`
-	Date     string        `json:"date"`
-	Rating   string        `json:"rating"`
-	Pic      string        `json:"pic"`
+	Title    string `json:"title"`
+	Desc     string `json:"desc"`
+	Location string `json:"location"`
+	Date     string `json:"date"`
+	Rating   string `json:"rating"`
+	Pic      string `json:"pic"`
 }
 
 func (m *NonMovie) GetTitle() string {
