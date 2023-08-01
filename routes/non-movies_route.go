721ef44012ef7332358d154da44e53632d5f6b17
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-01 11:11:46 +0800

    add post endpoint for creating Non-movie item

--- routes/non-movies_route.go
+++ routes/non-movies_route.go
@@ -9,4 +9,5 @@ import (
 func SetupNonMoviesRoutes(apiGroup *gin.RouterGroup) {
 	apiGroup.GET("/non-movies/details/:title", controllers.GetNonMovieByTitle())
 	apiGroup.GET("/non-movies", controllers.GetAllNonMovies())
+	apiGroup.POST("/non-movies", controllers.CreateNonMovie("Non-movies"))
 }
