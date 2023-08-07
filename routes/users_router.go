1af49bfa896f4a828b13b21cc61ed46f8dd05118
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-07 11:46:27 +0800

    add users_router

---
+++ routes/users_router.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"go-crud/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupUsersRoutes(apiGroup *gin.RouterGroup) {
+	apiGroup.POST("/user", controllers.CreateUser())
+}
