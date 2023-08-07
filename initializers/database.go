869d4f4fc25310621d418bd113dee1d939016e41
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-07 11:43:07 +0800

    connect to Postgres credential database

--- initializers/database.go
+++ initializers/database.go
@@ -7,13 +7,23 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var DB *mongo.Database
+var CRED_DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
 	dsn := os.Getenv("DB_URL")
+	cred_db := os.Getenv("CRED_URL")
+
+	CRED_DB, err = gorm.Open(postgres.Open(cred_db), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect credential database")
+	}
+	_ = CRED_DB
 
 	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(dsn))
 	if err != nil {
