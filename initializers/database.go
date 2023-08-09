b341fa0657909ef3769f19069fe15799c1d27c69
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-09 16:25:43 +0800

    update to use PostgreSQL database using GORM

--- initializers/database.go
+++ initializers/database.go
@@ -1,35 +1,22 @@
 package initializers
 
 import (
-	"context"
-	"log"
 	"os"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DB *mongo.Database
-var CRED_DB *gorm.DB
+var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := os.Getenv("DB_URL")
-	cred_db := os.Getenv("CRED_URL")
-
-	CRED_DB, err = gorm.Open(postgres.Open(cred_db), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect credential database")
-	}
-	_ = CRED_DB
-
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(dsn))
-	if err != nil {
-		log.Fatal(err)
-	}
+	db_url := os.Getenv("DB_URL")
 
 	// Get the database and collection
-	DB = client.Database("Movies")
+	DB, err = gorm.Open(postgres.Open(db_url), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	_ = DB
 }
