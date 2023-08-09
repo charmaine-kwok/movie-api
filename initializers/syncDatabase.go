95de44e566d665ca248cd2d1990708e3712ff050
charmaine-kwok <mickeymousekwok97@yahoo.com.hk>
2023-08-09 16:31:13 +0800

    add auto migration

--- initializers/syncDatabase.go
+++ initializers/syncDatabase.go
@@ -3,5 +3,7 @@ package initializers
 import "go-crud/models"
 
 func SyncDatabase() {
-	CRED_DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.Movie{})
+	DB.AutoMigrate(&models.NonMovie{})
 }
